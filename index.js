be11ff12ad0beb54098784b5b2ba74d47eefcad7
Kalam425 <180708762+Kalam425@users.noreply.github.com>
2025-04-24 01:52:59 -0400

    function/const created for player&comp for r,p,s

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("Hello World")
+
+const choices = [rock, paper, scissors]
+
+
+function game() {
+//this is a game
+//on the console only
+//for five rounds
+}
+
+function playRound(){
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+}
+
+function playerChoice () {
+    //input from player
+    let input = prompt("Type Rock, Paper, or Scissors");
+}
+
+function computerChoice () {
+    //random input for computer
+    return choices [Math.floor(Math.random()*choices.length)]
+    //the lowest "number" estimate of the 3 choices will ranbdomly be multiplied then chosen
+}
